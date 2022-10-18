7109cb719c85edf3bdfaf1ce75c6cb8f89197aee
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-10-18 19:45:31 +0800

    day2.java

---
+++ day2.java
@@ -0,0 +1,15 @@
+
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        String nama, nim, jurusan;
+        nama = "Taslim";
+        nim = "D0221056";
+        jurusan = "Teknik Informatika";
+        
+        System.out.println("Nama : " + nama + "\nNim : " + nim + "\nJurusan : " + jurusan);
+    }
+    
+}
