c6c0cbcf19da1faf31150f81abb1b9ddd3b2414a
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-10-26 19:49:12 +0800

    day10.java

---
+++ day10.java
@@ -0,0 +1,13 @@
+
+package day2;
+
+public class day10 {
+    public static void main(String[] args) {
+        String[] data = {"Jepa gollai", "onde-onde", "kambossol", "berres masing","bakwan","roti maros","loka janno","cendol","binte","jalankotenya mama ani"};
+    
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+            
+        }
+    }
+}
