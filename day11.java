65dc7153c709f84d25592e9be82d98e5a8172a40
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-10-27 08:46:28 +0800

    day11.java

---
+++ day11.java
@@ -0,0 +1,17 @@
+
+package day2;
+
+public class day11 {
+    public static void main(String[] args) {
+        int[] data = new int[5];
+        data [0] = 100;
+        data [1] = 90;
+        data [2] = 80;
+        data [3] = 70;
+        data [4] = 60;
+        
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+    }
+}
