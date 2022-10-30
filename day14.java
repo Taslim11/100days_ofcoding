66d78ca211b32c042f61e62719899def4314bede
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-10-30 22:57:39 +0800

    day14.java

---
+++ day14.java
@@ -0,0 +1,12 @@
+
+package day2;
+
+public class day14 {
+    public static void main(String[] args) {
+        boolean [] data = {true, false, false, true};
+        for(boolean nilai : data){
+            System.out.println(nilai);
+        }
+    }
+ 
+}
