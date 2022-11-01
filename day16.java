74be542deac6c4776982b7b5d22f9bfe36be04e7
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-11-01 22:29:51 +0800

    day16

---
+++ day16.java
@@ -0,0 +1,15 @@
+package day2;
+
+import java.util.Scanner;
+
+public class day16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nilai;
+        System.out.print("Nilai N = ");
+        nilai = sc.nextInt();
+        for (int i = 1; i <= nilai; i++) 
+            System.out.println("Angka ke "+ i);
+        
+    }
+}
