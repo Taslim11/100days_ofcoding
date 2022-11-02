f1caed06b9b17e3f934deba15ed9df3d305b8041
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-11-02 20:04:29 +0800

    day17.java

---
+++ day17.java
@@ -0,0 +1,17 @@
+
+package day2;
+
+public class day17 {
+    public static void main(String[] args) {
+        int a,b;
+        boolean hasil;
+        
+        a = 12;
+        b = 4;
+        
+        hasil = a > b;
+        System.out.println(hasil);
+        
+    }
+ 
+}
