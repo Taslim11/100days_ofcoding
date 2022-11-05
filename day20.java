f84079d9bd09d22e99fde42d2ee002b63fe0a3c9
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-11-05 19:53:42 +0800

    day20.java

---
+++ day20.java
@@ -0,0 +1,15 @@
+
+package day2;
+public class day20 {
+    public static void main(String[] args) {
+    boolean suka;
+    String jawaban;
+    
+    suka = true;
+    
+    jawaban = suka ? "iya" : "tidak";
+    
+        System.out.println(jawaban);
+    }
+    
+}
