416385d09031ae486445792a70d60860c665c482
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-11-09 22:30:09 +0800

    day24.java

---
+++ day24.java
@@ -0,0 +1,12 @@
+
+package day2;
+
+public class day24 {
+        public static void main(String[] args) {
+        int penghasilan, tabungan_tiapTahun, harga_mobil;
+            penghasilan = 13000000;
+            tabungan_tiapTahun = 5000000 * 12;
+            harga_mobil = 300000000;
+            System.out.println("jadi sambo harus menabung selama " + harga_mobil / tabungan_tiapTahun + " tahun");
+    }
+}
