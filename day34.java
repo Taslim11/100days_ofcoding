c0f5a648989205c073b8de8e0b4e8d28db8f6eb3
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-11-20 07:20:13 +0800

    day34.java

---
+++ day34.java
@@ -0,0 +1,51 @@
+
+package day2;
+
+import java.util.Scanner;
+
+public class day34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int menu, angka1, angka2;
+        
+        System.out.println("KALKULATOR");
+        System.out.println("1. Penjumlahan");
+        System.out.println("2. Pengurangan");
+        System.out.println("3. Perkalian");
+        System.out.println("4. Pembagian");
+        System.out.println("pilih menu :");
+        menu = sc.nextInt();  
+            if(menu == 1){
+                System.out.println("masukan angka pertama :");
+                    angka1 = sc.nextInt();
+                    System.out.println("masukan angka kedua :");
+                    angka2 = sc.nextInt();
+                    System.out.println("Hasilnya :");
+                    System.out.println(angka1+angka2 + 1);
+            }
+            else if(menu == 2){
+                System.out.println("masukan angka pertama :");
+                    angka1 = sc.nextInt();
+                    System.out.println("masukan angka kedua :");
+                    angka2 = sc.nextInt();
+                    System.out.println("Hasilnya :");
+                    System.out.println(angka1-angka2 - 1);
+            }
+            else if(menu == 3){
+                System.out.println("masukan angka pertama :");
+                    angka1 = sc.nextInt();
+                    System.out.println("masukan angka kedua :");
+                    angka2 = sc.nextInt();
+                    System.out.println("Hasilnya :");
+                    System.out.println(angka1*angka2 * 1);
+            }
+            else if(menu == 4){
+                System.out.println("masukan angka pertama :");
+                    angka1 = sc.nextInt();
+                    System.out.println("masukan angka kedua :");
+                    angka2 = sc.nextInt();
+                    System.out.println("Hasilnya :");
+                    System.out.println(angka1/angka2 - 1);
+            }
+    }
+}
