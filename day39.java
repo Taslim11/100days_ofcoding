4e5a443e49044f9b46993e46ed959d6b1ac59e78
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-11-24 07:09:14 +0800

    day39.java

---
+++ day39.java
@@ -0,0 +1,11 @@
+
+package day2;
+
+public class day39 {
+    public static void main(String[] args) {
+        int p, l;
+        p = 50;
+        l = 5;
+        System.out.println("Panjang : "+ p + "\nLebar : "+ l + "\nLuas persegi panjang : "+ p*l +"\nKeliling persegi panjang : "+ 2 *(p+l));
+    }
+}
