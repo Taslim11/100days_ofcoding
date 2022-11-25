bd431b838a957bd3dcfd3097fe9d8a69ad1ec18c
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-11-25 19:28:26 +0800

    day40.java

---
+++ day40.java
@@ -0,0 +1,19 @@
+
+package day2;
+
+import java.util.Scanner;
+
+public class day40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        System.out.print("Masukkan sisi a : ");
+        a = sc.nextInt();
+        System.out.print("Masukkan sisi b : ");
+        b = sc.nextInt();
+        System.out.print("Masukkan sisi c : ");
+        c = sc.nextInt();
+        System.out.println("Keliling Segitiga adalah : "+ (a + b + c));
+        
+    }
+}
