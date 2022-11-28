1e177f82732d029888c5beb95bcd545f83b543af
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-11-28 19:23:20 +0800

    day43.java

---
+++ day43.java
@@ -0,0 +1,13 @@
+
+package day2;
+
+import java.util.Scanner;
+
+public class day43 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan sisi kubus : ");
+        int sisi = sc.nextInt();
+        System.out.println("jadi keliling kubus adalah : "+ 12*sisi);
+    }
+}
