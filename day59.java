7ef79b884c078addc68ce6ad4e1965bb239fc76c
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-12-15 06:30:53 +0800

    day59.java

---
+++ day59.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class day59 {
+    public static void main(String[] args) {
+    double diameter = 10;
+    double tinggi = 5;
+    double luas = 2 * Math.PI * (diameter / 2) * (diameter / 2) + Math.PI * tinggi * diameter;
+    System.out.println("Luas tabung: " + luas);
+  }
+}
