d7fbcf3bbd9dc1bc9e45d29cc7b10ab39eb91231
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-12-15 06:37:23 +0800

    day60.java

---
+++ day60.java
@@ -0,0 +1,11 @@
+
+package day2;
+
+public class day60 {
+    public static void main(String[] args) {
+    double diameter = 10;
+    double keliling = 2 * Math.PI * (diameter / 2) + Math.PI * diameter;
+        System.out.println("Diamater tabung : "+ diameter);
+        System.out.println("Keliling tabung: " + keliling);
+  }
+}
