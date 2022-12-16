f69b6b349cd2d202cfeca3741d824d845b261cc5
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2022-12-16 10:31:40 +0800

    day61.java

---
+++ day61.java
@@ -0,0 +1,14 @@
+
+package day2;
+
+public class day61 {
+    public static void main(String[] args) {
+        double r = 10;
+        double tinggi = 20;
+        double pi = 3.14;
+        double keliling = 2 * pi * (r + tinggi);
+        
+            System.out.println("Keliling tabung adalah: " + keliling);
+  }
+}
+
