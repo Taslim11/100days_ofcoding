b00aaa43552cbbd91c3138b96cf58d516ca8891f
Taslim11 <99644613+Taslim11@users.noreply.github.com>
2023-01-25 19:04:51 +0800

    day100.java

---
+++ day100.java
@@ -0,0 +1,26 @@
+
+package day2;
+
+import java.util.Scanner;
+
+public class day100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hargaBinte = 10000;
+        int discount = 5000;
+        int jumlahPesanan, totalBayar;
+        
+        System.out.print("Masukkan jumlah pesanan :");
+        jumlahPesanan = sc.nextInt();
+        
+        if(jumlahPesanan >= 10){
+            totalBayar = hargaBinte * jumlahPesanan - discount;
+            System.out.println("total bayar : "+ totalBayar);
+        }
+        if(jumlahPesanan < 10){
+            totalBayar = hargaBinte * jumlahPesanan;
+            System.out.println("total bayar : "+ totalBayar);
+            
+        }
+    }
+}
